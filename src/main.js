56b9f5f196c769aab8abd8457015e808b069a097
zflexibles <75760504+zflexibles@users.noreply.github.com>
2020-12-09 17:46:21 -0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/** Sticky Navigation */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function(){
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+});
